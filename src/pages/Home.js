2a6aa67d75ac08ae3a42384058240864591d14eb
Bibek <admin@thedeveloperservice.com>
2021-12-16 18:10:49 +0545

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Products from '../components/Products';
+
+const Home = () => {
+    return (
+        <>
+        <div className="pb-24">
+            <Products />
+        </div>
+       </>
+    )
+}
+
+export default Home;
